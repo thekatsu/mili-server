2edf729f0c04d0b2151df2320d79883854e9dcac
Nicolas Fraga Faust <nicolas.faust@hotmail.com>
2024-03-24 19:26:45 -0300

    teste

--- app/inngest/client.ts
+++ app/inngest/client.ts
@@ -1,7 +1,7 @@
 import { Inngest } from 'inngest';
 
 // Create a client to send and receive events
-export const dynamic = 'force-dynamic';
+
 export const inngest = new Inngest({
   id: 'mili-server',
   env: process.env.BRANCH,
