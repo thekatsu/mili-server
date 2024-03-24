bb93bc6a75c40df282cb880fb6e5636b415b18c8
Nicolas Fraga Faust <nicolas.faust@hotmail.com>
2024-03-24 18:23:06 -0300

    try

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -5,9 +5,9 @@ import { importProducts } from '@/app/inngest/functions';
 export const dynamic = 'force-dynamic';
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  serveHost: 'https://mili-server.vercel.app',
+  serveHost: 'https://mili-server.vercel.app/',
   servePath: '/api/inngest',
   logLevel: 'debug',
-  baseUrl: '"https://api.inngest.com/',
+  baseUrl: 'https://api.inngest.com/',
   functions: [importProducts],
 });
