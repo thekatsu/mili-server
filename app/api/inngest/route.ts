55d8f99a009f42cf50149409a7591f8994b82b32
Nicolas Fraga Faust <nicolas.faust@hotmail.com>
2024-03-24 22:38:52 -0300

    teste

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -5,13 +5,6 @@ import { importProducts } from '@/app/inngest/functions';
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [importProducts],
-  signingKey:
-    'signkey-prod-c66c8beffbef0abee4c53dafb9c5ba1990a36c98684777c01feadeee7f55d618',
-  id: 'my-app',
-  logLevel: 'debug',
-  baseUrl: 'https://app.inngest.com',
-  serveHost: 'https://mili-server.vercel.app',
-  servePath: '/api/inngest',
 });
 
 // export const { GET, POST, PUT } = serve(ingest, {
--- app/inngest/client.ts
+++ app/inngest/client.ts
@@ -1,7 +1,7 @@
 import { Inngest } from 'inngest';
 
+// Create a client to send and receive events
+
 export const inngest = new Inngest({
   id: 'my-app',
-  eventKey:
-    'iEzkOL8Ni6KSydzrqq5SEh7tuDBWLKeFfkx7hQV0yySK5eOXzNm7SpwmUtoRgtUyL5iwGhnVhSJ3DOA1VueexQ',
 });
